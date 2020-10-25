222c3a5cc0641c8a23a292def3d5002ebdfe1d52
Guilherme H. Velten <guilherme-velten@live.com>
2020-10-25 16:55:15 -0300

    Alteraçãoes gerais.

--- SignalManager/Server/Controllers/UserController.cs
+++ SignalManager/Server/Controllers/UserController.cs
@@ -31,6 +31,13 @@ namespace SignalManager.Server.Controllers
             return await this.Http.PostJsonAsync<MiddlewareDomain<UserDomain>>("https://localhost:8080/user/fetch_async", middleware);
         }
 
+        [Microsoft.AspNetCore.Mvc.Route("RemoveBondAsync")]
+        [HttpPost]
+        public async Task<MiddlewareDomain<UserDomain>> RemoveBondAsync([FromBody] MiddlewareDomain<UserDomain> middleware)
+        {
+            return await this.Http.PostJsonAsync<MiddlewareDomain<UserDomain>>("https://localhost:8080/user/remove_bond_async", middleware);
+        }
+
         [Microsoft.AspNetCore.Mvc.Route("UpdateOrInsertAsync")]
         [HttpPost]
         public async Task<MiddlewareDomain<UserDomain>> UpdateOrInsertAsync([FromBody] MiddlewareDomain<UserDomain> middleware)
